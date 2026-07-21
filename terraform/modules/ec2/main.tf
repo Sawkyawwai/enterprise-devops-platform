85993cbe477f171711f3a947063d7b5df76f5288
Saw Kyaw Naing Wai <andrewchildlay@gmail.com>
2026-07-21 22:59:30 +0800

    Format Terraform code

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,14 +1,14 @@
 resource "aws_instance" "this" {
 
-  ami                    = var.ami_id
+  ami = var.ami_id
 
-  instance_type          = var.instance_type
+  instance_type = var.instance_type
 
-  subnet_id              = var.subnet_id
+  subnet_id = var.subnet_id
 
   vpc_security_group_ids = [var.security_group_id]
 
-  key_name               = var.key_name
+  key_name = var.key_name
 
   associate_public_ip_address = true
 
